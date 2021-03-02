f13e99b3c87a1926b2b00964310805f9c3175c34
Eduardo Khattab <eduardo.khattab@gympass.com>
2021-03-02 19:35:11 -0300

    feat: add json desserializer

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,18 +1,25 @@
 package br.com.alura.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (var i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = key + ",432432,343243";
-                var email = "Welcome, we are processing your order!";
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
 
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var order = new Order(userId, orderId, amount);
+
+                    var email = "Welcome, we are processing your order!";
+
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
